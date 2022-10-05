cd182325dbf5fdda99068b9ddcfc7ae027ecacbe
Andreas Hjorhøy Rasmussen <88768828+Andrula@users.noreply.github.com>
2022-10-05 13:35:54 +0200

    Co-authored-by: NicklasElBeik <NicklasElBeik@users.noreply.github.com>

--- engine.js
+++ engine.js
@@ -4,23 +4,19 @@ window.onload = function () {
 
   var categories;         // Array of topics
   var chosenCategory;     // Selected catagory
-  var word ;              // Selected word
-  var guess ;             // Geuss
-  var geusses = [ ];      // Stored geusses
-  var counter ;           // Count correct geusses
+  var word;              // Selected word
+  var guess;             // Geuss
+  var geusses = [];      // Stored geusses
+  var counter;           // Count correct geusses
   var space;              // Number of spaces in word '-'
   var getSpin = "";
   var totalPrize = 0;
   var isSpun = false;
-  var errormsg = "";
   var fullGuess = "";
-  
+
   // Get elements
   var showSpin = document.getElementById("myspin");
   var showTotal = document.getElementById("mytotal");
-  var showError = document.getElementById("error");
-  var showWin = document.getElementById("showwin");
-
 
 
   // create alphabet ul
@@ -39,32 +35,10 @@ window.onload = function () {
     }
   }
 
-  function get_random (list) {
-      return list[Math.floor((Math.random()*list.length))];
-    }
+  function get_random(list) {
+    return list[Math.floor((Math.random() * list.length))];
+  }
 
-  document.getElementById('spin').onclick = function() {
-      if(isSpun) {
-          errormsg = "Du må kun spinne én gang!"
-          showError.innerHTML = errormsg;
-      }
-      else {
-      errormsg = "";
-      showError.innerHTML = errormsg;
-      getSpin = get_random(prize);
-      isSpun = true;
-      }
-      if (getSpin == 'Fallit') {
-        errormsg = "Du er desværre gået fallit og har mistet puljen. Prøv igen!"
-        showError.innerHTML = errormsg;
-        totalPrize = 0;
-        getSpin = "";
-        isSpun = false;
-      }
-      comments();
-    }
-    
-  
   // Select Catagory
   var selectCat = function () {
     if (chosenCategory === categories[0]) {
@@ -78,8 +52,28 @@ window.onload = function () {
     }
   }
 
+  document.getElementById('spin').onclick = function () {
+    if (isSpun) {
+      showTotal.innerHTML = "Du må kun spinne én gang!"
+      setTimeout(function () {
+        comments();
+      }, 2000);
+    }
+    else {
+      getSpin = get_random(prize);
+      isSpun = true;
+      comments();
+    }
+    if (getSpin == 'Fallit') {
+      showTotal.innerHTML = "Du er gået fallit og har mistet dine penge. Prøv igen!"
+      totalPrize = 0;
+      getSpin = "";
+      isSpun = false;
+    }
+  }
+
   // Create geusses ul
-   result = function () {
+  result = function () {
     wordHolder = document.getElementById('hold');
     correct = document.createElement('ul');
 
@@ -93,49 +87,44 @@ window.onload = function () {
       } else {
         guess.innerHTML = "_";
       }
-
       geusses.push(guess);
       wordHolder.appendChild(correct);
       correct.appendChild(guess);
     }
   }
-  
+
   // Show lives
-   comments = function () {
+  comments = function () {
     showTotal.innerHTML = "Du har " + totalPrize + " kr.";
     showSpin.innerHTML = getSpin;
     for (var i = 0; i < geusses.length; i++) {
       if (counter + space === geusses.length) {
-        showWin.innerHTML = "Tillykke, du har vundet " + totalPrize + " kr.!";
+        showTotal.innerHTML = "Tillykke, du har vundet " + totalPrize + " kr.!";
       }
     }
   }
 
-  document.getElementById('submitGuess').onclick = function() {
+  document.getElementById('submitGuess').onclick = function () {
     var fullGuess = document.getElementById("fullGuess").value;
     console.log(fullGuess);
     if (fullGuess.toUpperCase() == word.toUpperCase()) {
-      showWin.innerHTML = "Tillykke, du har vundet " + totalPrize + " kr.!";
+      showTotal.innerHTML = "Tillykke, du har vundet " + totalPrize + " kr.!";
       for (var i = 0; i < word.length; i++) {
-          geusses[i].innerHTML = word[i];
-           counter += 1;
-          // totalPrize = totalPrize + getSpin;
-          // getSpin = "";
+        geusses[i].innerHTML = word[i];
+        counter += 1;
       }
     }
     else {
-      var errormsg = "Du gættede desværre forkert.";
-      showError.innerHTML = errormsg;
+      showTotal.innerHTML = "Du gættede desværre forkert."
+      setTimeout(function () {
+        comments();
+      }, 2000);
     }
   }
-
-
 
   // OnClick Function
   check = function () {
     list.onclick = function () {
-      errormsg = "";
-      showError.innerHTML = errormsg;
       if (isSpun) {
         var geuss = (this.innerHTML);
         this.setAttribute("class", "active");
@@ -146,6 +135,7 @@ window.onload = function () {
             counter += 1;
             totalPrize = totalPrize + getSpin;
             getSpin = "";
+            comments();
           }
           var index = alphabet.indexOf(geuss);
           if (index > -1)// only splice array when item is found
@@ -157,22 +147,24 @@ window.onload = function () {
         }
       }
       else {
-        errormsg = "Du skal spinne først!";
-        showError.innerHTML = errormsg;
+        showTotal.innerHTML = "Du skal spinne først!"
+        setTimeout(function () {
+          comments();
+        }, 2000);
       }
-      comments();
     }
+
     isSpun = false;
   }
-  
-    
+
+
   // Play
   play = function () {
     categories = [
-        ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester city", "newcastle united"],
-        ["alien", "dirty harry", "gladiator", "finding nemo", "jaws"],
-        ["manchester", "milan", "madrid", "amsterdam", "prague"],
-        ["javascript","php","rust","python","java","ruby","sql"]
+      ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester city", "newcastle united"],
+      ["alien", "dirty harry", "gladiator", "finding nemo", "jaws"],
+      ["manchester", "milan", "madrid", "amsterdam", "prague"],
+      ["javascript", "php", "rust", "python", "java", "ruby", "sql"]
     ];
 
     chosenCategory = categories[Math.floor(Math.random() * categories.length)];
@@ -183,15 +175,14 @@ window.onload = function () {
 
 
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+      't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
     buttons();
-    errormsg = "";
-    showError.innerHTML = errormsg;
+ 
     isSpun = false;
     totalPrize = 0
-    geusses = [ ];
+    geusses = [];
     counter = 0;
     space = 0;
     result();
@@ -202,13 +193,17 @@ window.onload = function () {
   play();
 
 
-   // Reset
+  // Reset
 
-  document.getElementById('reset').onclick = function() {
+  document.getElementById('reset').onclick = function () {
     correct.parentNode.removeChild(correct);
     letters.parentNode.removeChild(letters);
     play();
   }
+
+
+
+
 }
 
 
